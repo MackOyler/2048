3d07586d26cf7aea6611e5ba91f5582c9b9d1b8c
Mack Oyler <120589371+MackOyler@users.noreply.github.com>
2024-04-30 17:00:45 -0500

    made grid

--- main.py
+++ main.py
@@ -24,6 +24,60 @@ MOVE_VEL = 20
 WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("2048")
 
+
+class Tile:
+    COLORS = [
+        (237, 229, 218),
+        (238, 225, 201),
+        (243, 178, 122),
+        (246, 150, 101),
+        (247, 124, 95),
+        (247, 95, 59),
+        (237, 208, 115),
+        (237, 204, 99),
+        (236, 202, 80)
+    ]
+    
+    def __init__(self, value, row, col):
+        self.value = value
+        self.row = row
+        self.col = col
+        self.x = col * RECT_WIDTH
+        self.y = row * RECT_HEIGHT
+        
+    def get_color(self):
+        pass
+    
+    def draw(self, window):
+        pass
+    
+    def set_pos(self):
+        pass
+    
+    def move(self, delta):
+        pass
+
+
+def draw_grid(window):
+    for row in range(1, ROWS):
+        y = row * RECT_HEIGHT
+        pygame.draw.line(window, OUTLINE_COLOR, (0, y), (WIDTH, y), OUTLINE_THICKNESS)
+    
+    pygame.draw.rect(window, OUTLINE_COLOR, (0, 0, WIDTH, HEIGHT), OUTLINE_THICKNESS)
+    
+    for col in range(1, COLS):
+        x = col * RECT_WIDTH
+        pygame.draw.line(window, OUTLINE_COLOR, (x, 0), (x, HEIGHT), OUTLINE_THICKNESS)
+    
+    pygame.draw.rect(window, OUTLINE_COLOR, (0, 0, WIDTH, HEIGHT), OUTLINE_THICKNESS)
+
+def draw(window):
+    window.fill(BACKGROUND_COLOR)
+    
+    draw_grid(window)
+    
+    pygame.display.update()
+
 def main(window):
     clock = pygame.time.Clock()
     run = True
@@ -35,6 +89,8 @@ def main(window):
             if event.type == pygame.QUIT:
                 run = False
                 break
+        
+        draw(window)    
             
     pygame.quit()
 
