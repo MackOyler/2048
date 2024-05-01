6ce0d588d5f49a9511e9b0b8d3adad8e6531f654
Mack Oyler <120589371+MackOyler@users.noreply.github.com>
2024-05-01 11:09:11 -0500

    implementing moving tiles

--- main.py
+++ main.py
@@ -67,7 +67,8 @@ class Tile:
         pass
     
     def move(self, delta):
-        pass
+        self.x += delta[0]
+        self.y += delta[1]
 
 
 def draw_grid(window):
@@ -93,20 +94,81 @@ def draw(window, tiles):
     
     pygame.display.update()
     
-    def  generate_tiles():
-        tiles = {}
-        for _ in range(2):
-            row, col = get_random_pos(tiles)
-            tiles[f"{row}{col}"] = Tile(2, row, col)
+def get_random_pos(tiles):
+    row = None
+    col = None
+    while True:
+        row = random.randrange(0, ROWS)
+        col = random.randrange(0, COLS)
+        
+        if f"{row}{col}" not in tiles:
+            break
+        
+    return row, col
+
+def move_tiles(window, tiles, clock, direction):
+    updated = True
+    blocks = set()
+    
+    if direction == "left":
+        sort_func = lambda x: x.col
+        reverse = False
+        delta = (-MOVE_VEL, 0)
+        boundary_check = lambda tile: tile.col == 0
+        get_next_tile = lambda tile: tiles.get(f"{tile.row}{tile.col - 1}")
+        merge_check = lambda tile, next_tile: tile.x > next_tile.x + MOVE_VEL
+        move_check = lambda tile, next_tile: tile.x > next_tile.x + RECT_WIDTH + MOVE_VEL
+        ceil = True
+        
+    elif direction == "right":
+        pass
+    elif direction == "up":
+        pass
+    elif direction == "down":
+        pass
+    
+    while updated:
+        clock.tick(FPS)
+        updated = False
+        sorted_tiles = sorted(tiles.values(), key=sort_func, reverse=reverse)
+        
+        for i, tile in enumerate(sorted_tiles):
+            if boundary_check(tile):
+                continue
             
-        return tiles
+            next_tile = get_next_tile(tile)
+            if not next_tile:
+                tile.move(delta)
+            elif (
+                tile.value == next_tile.value
+                and tile not in blocks 
+                and next_tile not in blocks
+            ): 
+                if merge_check(tile, next_tile):
+                    tile.move(delta)
+                else:
+                    next_tile.value *= 2
+                    sorted_tiles.pop(i)
+                    blocks.add(next_tile)
+            elif move_check(tile, next_tile):
+                tile.move(delta)
+            else:
+                continue
+    
+def  generate_tiles():
+    tiles = {}
+    for _ in range(2):
+        row, col = get_random_pos(tiles)
+        tiles[f"{row}{col}"] = Tile(2, row, col)
+            
+    return tiles
 
 
 def main(window):
     clock = pygame.time.Clock()
     run = True
     
-    tiles = {}
+    tiles = generate_tiles()
     
     while run: 
         clock.tick(FPS)
